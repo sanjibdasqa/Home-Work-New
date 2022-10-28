f0e3779e815dee854c5b1d0669fbc7f4472e04e7
sanjibdasqa <sanjibdasqa@gmail.com>
2022-10-27 18:42:54 -0800

    hw new

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -1,7 +1,8 @@
 package hw4JavaVariables;
 
 public class AboutMe {
-	
+	// variables are declared here
+
 	public String name;
 	public byte age;
 	public short myHouseNo;
@@ -11,23 +12,21 @@ public class AboutMe {
 	public double myGpa;
 	public char sex;
 	public boolean carLicense;
-	
+
+	// Here Constructor declared
+
 	public AboutMe() {
 		System.out.println("This is all about us");
-		
+
 	}
-	
+
+	// Here method implemented
+
 	public void aboutme() {
-		
-		System.out.println("My Name: " + name + "\nMy Age: " + age + "\nMy House No: " + myHouseNo
-				+ "\nMy Car No:"+ myCarNo + "\nMy Phone No" + myPhoneNo + "\nMySex:" + sex
-				+ "\nMy Weight: " + myWeight + "\nMy Grade: " + myGpa +"\nDo I Have Any Driving License ? Ans: " + carLicense);
-	}
-			
-			
-	
-	
-	}
-	
 
+		System.out.println("My Name: " + name + "\nMy Age: " + age + "\nMy House No: " + myHouseNo + "\nMy Car No:"
+				+ myCarNo + "\nMy Phone No" + myPhoneNo + "\nMySex:" + sex + "\nMy Weight: " + myWeight + "\nMy Grade: "
+				+ myGpa + "\nDo I Have Any Driving License ? Ans: " + carLicense);
+	}
 
+}
--- src/hw4JavaVariables/AboutMeTest.java
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -1,41 +1,37 @@
 package hw4JavaVariables;
 
-
 public class AboutMeTest {
 	public static void main(String[] args) {
-		AboutMe sanjib = new AboutMe();
-		
-		sanjib.name ="Sanjib Das";
+		AboutMe sanjib = new AboutMe(); // Constructor is initialized here
+		// Variables are initialized here
+		sanjib.name = "Sanjib Das";
 		sanjib.age = 89;
 		sanjib.myHouseNo = 25478;
 		sanjib.myCarNo = 25478962;
 		sanjib.myPhoneNo = 547863214554875l;
 		sanjib.myWeight = 1.145874f;
-		sanjib.myGpa =3.84565566125;
+		sanjib.myGpa = 3.84565566125;
 		sanjib.sex = 'M';
 		sanjib.carLicense = false;
-		
-		sanjib.aboutme();
-		
+
+		sanjib.aboutme(); // Method is Initialized
+
 		System.out.println("\n----------------------------------------------------\n");
-		
-		AboutMe sanjib01=new AboutMe();
-		
-		sanjib01.name ="Alex";
+
+		AboutMe sanjib01 = new AboutMe();
+		// Variables are initialized here
+		sanjib01.name = "Alex";
 		sanjib01.age = 52;
 		sanjib01.myHouseNo = 24789;
 		sanjib01.myCarNo = 2547892;
 		sanjib01.myPhoneNo = 587863254854875l;
 		sanjib01.myWeight = 2.245874f;
-		sanjib01.myGpa =2.25565566125;
+		sanjib01.myGpa = 2.25565566125;
 		sanjib01.sex = 'M';
 		sanjib01.carLicense = false;
-		
-		sanjib01.aboutme();
-		
-		
-		
-		
+
+		sanjib01.aboutme(); // Method is Initialized
+
 	}
 
 }
